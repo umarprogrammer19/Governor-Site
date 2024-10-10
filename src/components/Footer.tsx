ea2e7fad634ff63a79bef8a9de1cf3762ab964ba
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-10 16:04:23 +0500

    Improved Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
                             <li>Earn as You Learn</li>
                         </ul>
                     </div>
-                    <div className='flex mr-24 gap-10 md:flex-row flex-col'>
+                    <div className='flex gap-10 md:flex-row flex-col'>
                         <ul className='flex flex-col gap-3'>
                             <h4 className='font-bold text-black text-2xl md:mb-4'>Advanced Courses</h4>
                             <li>Artificial Intelligence</li>
@@ -25,6 +25,8 @@ const Footer = () => {
                             <li>Genomics and Bioinformatics</li>
                             <li>Network Programmability and Automation</li>
                         </ul>
+                    </div>
+                    <div>
                         <div>
                             <h4 className='font-bold text-black text-2xl mb-6'>Social Links</h4>
                             <ul className='flex gap-4 items-center mb-4'>
@@ -34,7 +36,10 @@ const Footer = () => {
                                 <Image src='/instagram.png' alt='istagram' width={30} height={30} />
                                 <Image src='/tiktok.png' alt='tiktok' width={30} height={30} />
                             </ul>
-                            <Link href='mailto:education@governorsindh.com'><li>education@governorsindh.com</li></Link>
+                            <Link href='mailto:education@governorsindh.com'><li className='list-none text-blue-600 underline'>
+                            <Image src='https://cdn-icons-png.freepik.com/512/2250/2250206.png' alt='Icon'></Image>
+                            <span>education@governorsindh.com</span>
+                            </li></Link>
                         </div>
                     </div>
                 </div>
